0f2e5047998b9a749d618d21e1e0fa249ed8e038
endryuman <132812966+endryuman@users.noreply.github.com>
2024-01-06 19:20:38 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,48 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+  }
+  abstract openDoor(key: Key): void;
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(enteredKey: Key): void {
+    if (enteredKey.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +51,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
